6f9e954de06ffa718e7cb7ba4c6a2f6522cf3f82
Sveta Krivosheeva <krivosheeva.svetlana.s@gmail.com>
2024-07-15 18:55:01 +0600

    add proxyman

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences_tools/shared_preferences_tools.dart';
 
@@ -5,6 +7,7 @@ import 'screens/home/home_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  HttpOverrides.global = MyHttpOverrides();
   await SharedPreferencesToolsDebug.init();
   runApp(const MyApp());
 }
@@ -27,5 +30,14 @@ class MyApp extends StatelessWidget {
       ),
       home: const HomePage(),
     );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
   }
 }
